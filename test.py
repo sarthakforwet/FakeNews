6e318ad26bd587f649563f61c522176ec9dcce04
sarthakforwet <sarthakkhandelwal032000@gmail.com>
2020-03-29 04:53:30 +0530

    Added text and analyze feature in test.py

--- test.py
+++ test.py
@@ -5,6 +5,10 @@ from PIL import Image
 import argparse
 
 label = {True:"CorrectNews",False:"FakeNews"}
+
+# Loading our Model.
+clf = pickle.load(open("FakeNewsDetection.pkl","rb"))
+ 
 def calculate_result(test_sentence):
     output = clf.predict(test_sentence)[0]
     proba = clf.predict_proba(test_sentence)[0]
@@ -27,21 +31,24 @@ def preprocess_image(sentence):
 # Parse Argument for the image.
 parser = argparse.ArgumentParser()
 parser.add_argument("--image",help="The image which is to be tested")
+parser.add_argument("--text",help="The sentence or group of sentences which are to be tested")
+parser.add_argument("analyze",help="Analyze the model",default=True)
 args = parser.parse_args()
 
-# Loading our Model.
-clf = pickle.load(open("FakeNewsDetection.pkl","rb"))
-
-test_sentence = ["Various shops and schools are closed due to corona-virus"]
-calculate_result(test_sentence)
-
-# Lets try it for Image
-img = Image.open("testImg_model.jpeg")
-sentence = pytesseract.image_to_string(img)
-test_sentence = preprocess_image(sentence)
-calculate_result([test_sentence])
+if args.analyze=="True":
+    test_sentence = ["America will soon be on its kmees"]
+    calculate_result(test_sentence)
+    
+    # Lets try it for Image
+    img = Image.open("testImg_model.jpeg")
+    sentence = pytesseract.image_to_string(img)
+    test_sentence = preprocess_image(sentence)
+    calculate_result([test_sentence])
 
 if args.image:
     sentence = pytesseract.image_to_string(args.image)
     test_sentence = preprocess_image(sentence)
     calculate_result([test_sentence])
+
+if args.text:
+    calculate_result([args.text])
